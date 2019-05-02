a66d440492f280433278bf6b8ddcaaa7daaad250
tharinduvindula <tharinduvindula@gmail.com>
2019-05-02 07:25:22 +0530

    v1.0 2019050201 admin part conect with frontend

--- routes/api.php
+++ routes/api.php
@@ -25,5 +25,8 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+    Route::get('getalluser', 'AuthController@getalluser');
+    Route::post('getuser', 'AuthController@getuser');
+
 
 });
