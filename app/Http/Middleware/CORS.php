0df88c993586b8d53533c64cecd6f3a8860fe9c3
tharinduvindula <tharinduvindula@gmail.com>
2020-02-24 12:55:58 +0530

    add cors origin

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -15,8 +15,9 @@ class CORS
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin : *');
-        header('Access-Control-Allow-Headers : Content-type, X-Auth-Token, Authorization, Origin');
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
     }
 }
