e3bde8979c31cf6911fc5154a2c66ee229ca296a
Geesa-Vihara <geesavihara97@gmail.com>
2020-03-04 11:01:57 +0530

    v1.0 20200304 01 modify deceased

--- app/Http/Controllers/deceased.php
+++ app/Http/Controllers/deceased.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -12,13 +14,14 @@ class deceased extends Controller
         DB::table('police')->insert([
         "srjno" => $request->input('srjno'),
     	"policefullname" => $request->input('policefullname'),
-        "policetagno" => $request->input('policetag'),
+        "policetag" => $request->input('policetag'),
         "policearea" => $request->input('policearea'),
     	"policerank" => $request->input('policerank'),
-    	"poliecphoneno" => $request->input('policephoneno'),
+    	"policephoneno" => $request->input('policephoneno'),
     	"policescenephoto" => $request->input('policescenephoto'),
     	"policefoldername" => $request->input('policefoldername'),
-
+        'created_at' => Carbon::now(),
+        'updated_at' => Carbon::now()
 
 
         ]);
@@ -32,7 +35,8 @@ class deceased extends Controller
             "coronerfullname" => $request->input('coronerfullname'),
             "coronerarea" => $request->input('coronerarea'),
             "coronerordergivenby" => $request->input('coronerordergivenby'),
-
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
 
 
             ]);
@@ -48,7 +52,8 @@ class deceased extends Controller
             "sex" => $request->input('sex'),
             "address" => $request->input('address'),
             "contactnumber" => $request->input('contactnumber'),
-
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
 
 
             ]);
